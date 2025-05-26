206f12343f311600a1ad086b0a24bfb863515a40
Yukai <keeyukai98@gmail.com>
2025-05-26 15:58:15 +0800

    completed display album page to show songs in albums

--- src/components/DisplayAlbum.jsx
+++ src/components/DisplayAlbum.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import NavBar from './NavBar'
 import { useParams } from 'react-router-dom'
-import { albumsData, assets } from '../assets/assets'
+import { albumsData, assets, songsData } from '../assets/assets'
 
 
 const DisplayAlbum = () => {
 
         const {id} = useParams();
-        const albumData = albumsData[id]
+        const albumData = albumsData[id];
         console.log(albumData)
 
     return (
@@ -17,14 +17,47 @@ const DisplayAlbum = () => {
                 <img className = 'w-48 rounded'  src={albumData.image} alt=""/> 
                 <div className ='flex flex-col'>
                     <p>Playlist</p>
-                    <h2 className ='' >albumData.name</h2>
-                    <h4>albumData.desc</h4>
-                    <p>
-                        <img className= '' src={assets.spotify_logo} alt=""/>
+                    <h2 className ='text-5xl font-bold mb-4 md:text-7xl' >{albumData.name}</h2>
+                    <h4>{albumData.desc}</h4>
+                    <p className='mt-1'>
+                        <img className= 'inline-block w-5' src={assets.spotify_logo} alt=""/>
                         <b>Spotify</b>
+                        • 123123123 likes
+                        • <b>50 songs,</b>
+                        about album duration
                     </p>
                 </div>
             </div>
+            <div className='grid grid-cols-3 sm:grid-cols-4 mt-10 mb-4 pl-2 text[#a7a7a7]'>
+                <p><b className='mr-4'>#</b>Title</p>
+                <p>Album</p>
+                <p className='hidden sm:block'>Date added</p>
+                <img className='m-auto w-4' src={assets.clock_icon} alt=''/>
+            </div>
+            <hr />
+            {
+                songsData.map((item,index)=>(
+                    <div key={index} className='grid grid-cols-3 sm:grid-cols-4 gap-2 p-2 items-center text-[#a7a7a7] hover:bg-[#ffffff2b] cursor-pointer'>
+                        <p className='text-white'>
+                            <b className='mr-4 text-[#a7a7a7]'>{index+1}</b>
+                            <img className='inline w-10 mr-5' src={item.image} alt=''/>
+                            {item.name}
+                        </p>
+                        <p className='text-[15px]'>
+                            {albumData.name}
+                        </p>
+                        <p className='text-[15px] hidden sm:block'>
+                            x days ago
+                        </p>
+                        <p className='text-[15px] text-center'>
+                            {item.duration}
+                        </p>
+                    </div>
+                )
+            
+            )
+            }
+            
         </>
     )
 }
